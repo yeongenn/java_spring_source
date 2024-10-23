18cd25e1f49a68e38a4ba88d2bb5fb79854a6458
yeongenn <yngn0012@naver.com>
2024-10-23 22:49:46 +0900

    docs: LoginCheck.java 주석 추가

--- testauth/src/main/java/pack/config/LoginCheck.java
+++ testauth/src/main/java/pack/config/LoginCheck.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * @LoginCheck : 접근하는 사용자가 로그인 했는지 여부 체크
+ */
+
 @Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LoginCheck {
